e658a6514e754906d094fecc414058d581154bc2
Leandro Borges Ferreira <lehen01@gmail.com>
2023-05-11 22:07:06 +0200

    Fixing BackStackManager and adding unit tests

--- storyteller/src/main/java/br/com/leandroferreira/storyteller/backstack/BackStackManager.kt
+++ storyteller/src/main/java/br/com/leandroferreira/storyteller/backstack/BackStackManager.kt
@@ -1,53 +1,97 @@
 package br.com.leandroferreira.storyteller.backstack
 
+import br.com.leandroferreira.storyteller.model.backtrack.AddText
 import br.com.leandroferreira.storyteller.model.change.TextEditInfo
 import java.util.Stack
 
-private const val DEFAULT_BACK_TEXT_LIMIT = 10
+private const val HAS_SPACE_REGEX = "\\s\\S"
 
-class BackStackManager(private val backTextLimit: Int = DEFAULT_BACK_TEXT_LIMIT) {
-
+class BackStackManager {
 
     private val backStack: Stack<Any> = Stack()
-    private val preStack: Stack<Any> = Stack()
     private val forwardStack: Stack<Any> = Stack()
 
-    private var currentTextPosition: Int? = null
-
+    /**
+     * Add action evaluates if the action is a whole action, like move, delete, merge content or
+     * a non whole action, like editing a text. If the action is not whole, it merges the last
+     * action with the new one in the stack. If the action is whole, it simply adds it to the
+     * backstack.
+     */
     fun addAction(action: Any) {
-        forwardStack.clear()
-        backStack.add(action)
+        if (backStack.isEmpty()) {
+            addActionWhenEmpty(action)
+            return
+        }
+
+        if (action !is TextEditInfo) {
+            addWhenWholeAction(action)
+            return
+        }
+
+        handleNewTextAdded(action)
     }
 
     fun backAction(): Any = backStack.pop().also(forwardStack::add)
 
     fun forwardAction(): Any = forwardStack.pop().also(backStack::add)
 
-    /**
-     * This method searches for the last action in the backStack. When the first item is a whole
-     * action, like move, delete and merge information, it simply takes the first element and pops
-     * it. But when the first items of the stack and TextEditInfo, it search for the last typed
-     * word and pops all last word as an action.
-     */
-    private fun popAction(): Any {
-        if (backStack.peek() !is TextEditInfo) return backStack.pop()
+    fun peek(): Any = backStack.peek()
 
-        val lastActions = mutableListOf<TextEditInfo>()
-        val lastPosition = (backStack.peek() as TextEditInfo).position
+    private fun addActionWhenEmpty(action: Any) {
+        forwardStack.clear()
 
-        var acc = 0
-
-        while (shouldContinueSearch(backStack.peek(), lastPosition) && acc < backTextLimit) {
-            lastActions.add(backStack.pop() as TextEditInfo)
-            acc++
+        if (action is TextEditInfo) {
+            backStack.add(
+                AddText(
+                    text = action.text,
+                    position = action.position,
+                    isComplete = action.text.contains(HAS_SPACE_REGEX.toRegex())
+                )
+            )
+        } else {
+            backStack.add(action)
         }
-
-        val textToErase = lastActions.joinToString(separator = "")
-        return TextEditInfo(text = textToErase, position = lastPosition)
     }
 
-    private fun shouldContinueSearch(nextElement: Any, lastPosition: Int): Boolean =
-        nextElement is TextEditInfo &&
-            nextElement.position == lastPosition &&
-            !nextElement.text.contains(" ")
+    private fun addWhenWholeAction(action: Any) {
+        val lastAction = backStack.peek()
+        if (lastAction is AddText && !lastAction.isComplete) {
+            val popped = backStack.pop()
+            backStack.add((popped as AddText).copy(isComplete = true))
+        }
+
+        forwardStack.clear()
+        backStack.add(action)
+    }
+
+    private fun handleNewTextAdded(action: TextEditInfo) {
+        val lastAction = backStack.peek()
+
+        if (lastAction is AddText &&
+            !lastAction.isComplete &&
+            action.position == lastAction.position
+        ) {
+            val popped = backStack.pop() as AddText
+            val newText = "${popped.text}${action.text}"
+            val isComplete = newText.contains(HAS_SPACE_REGEX.toRegex())
+
+            if (!isComplete) {
+                val newAction = popped.copy(
+                    text = newText,
+                    isComplete = false
+                )
+                backStack.add(newAction)
+            } else {
+                val newAction = popped.copy(isComplete = true)
+                backStack.add(newAction)
+                backStack.add(
+                    AddText(
+                        text = action.text,
+                        position = action.position,
+                        isComplete = false
+                    )
+                )
+            }
+        }
+    }
 }
