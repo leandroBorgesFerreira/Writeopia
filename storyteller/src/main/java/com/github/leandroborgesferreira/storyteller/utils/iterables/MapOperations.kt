81599631cd524ddeae8285f153af661b54552085
Leandro Borges Ferreira <lehen01@gmail.com>
2023-06-20 19:33:13 +0200

    Map merge with tests

--- storyteller/src/main/java/com/github/leandroborgesferreira/storyteller/utils/iterables/MapOperations.kt
+++ storyteller/src/main/java/com/github/leandroborgesferreira/storyteller/utils/iterables/MapOperations.kt
@@ -3,6 +3,17 @@ package com.github.leandroborgesferreira.storyteller.utils.iterables
 object MapOperations {
 
     fun <T> mergeSortedMaps(originalMap: Map<Int, T>, newMap: Map<Int, T>): Map<Int, T> {
-        return originalMap.toMutableMap().apply { putAll(newMap) }
+        val mutable = originalMap.mapKeys { (key, _) -> key * 10 }.toMutableMap()
+
+        newMap.mapKeys { (key, _) -> key * 10 }.forEach { (key, value) ->
+            if (mutable[key] != null) {
+                mutable[key - 1] = value
+            } else {
+                mutable[key] = value
+            }
+        }
+
+        var acc = 0
+        return mutable.toSortedMap().mapKeys { acc++ }
     }
 }
