4aaa0b8259e13cdf0b64f6d4217a3f5a42bac7cf
Leandro Borges Ferreira <lehen01@gmail.com>
2023-06-29 18:49:09 +0200

    Removing unused imports

--- app_sample/src/main/java/br/com/leandroferreira/app_sample/screens/menu/ui/screen/ConfigurationsMenu.kt
+++ app_sample/src/main/java/br/com/leandroferreira/app_sample/screens/menu/ui/screen/ConfigurationsMenu.kt
@@ -1,6 +1,5 @@
 package br.com.leandroferreira.app_sample.screens.menu.ui.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -26,13 +25,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.leandroferreira.app_sample.R
-import br.com.leandroferreira.app_sample.screens.menu.viewmodel.ChooseNoteViewModel
 import br.com.leandroferreira.app_sample.views.SlideInBox
 
 private const val INNER_PADDING = 3
