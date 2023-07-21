7fa5a22192294b60843906dd3d7bf6a607e5c89f
Leandro Borges Ferreira <lehen01@gmail.com>
2023-07-21 22:03:43 +0200

    Small code move

--- storyteller/src/main/java/com/github/leandroborgesferreira/storyteller/model/action/Action.kt
+++ storyteller/src/main/java/com/github/leandroborgesferreira/storyteller/model/action/Action.kt
@@ -9,12 +9,12 @@ sealed class Action {
     data class BulkDelete(val deletedUnits: Map<Int, StoryStep>) : Action()
     data class Check(val storyUnit: StoryStep, val position: Int, val checked: Boolean)
     data class LineBreak(val storyStep: StoryStep, val position: Int)
+    data class Move(val storyUnit: StoryStep, val positionFrom: Int, val positionTo: Int)
+
     data class Merge(
         val receiver: StoryStep,
         val sender: StoryStep,
         val positionFrom: Int,
         val positionTo: Int
     )
-
-    data class Move(val storyUnit: StoryStep, val positionFrom: Int, val positionTo: Int)
 }
