ab9f11d8f5ffcbfb3108bf3efd6b9527cee3c443
Leandro Borges Ferreira <lehen01@gmail.com>
2023-07-23 07:20:01 +0200

    Deleting MoveInfo

--- storyteller/src/main/java/com/github/leandroborgesferreira/storyteller/model/action/MoveInfo.kt
+++
@@ -1,5 +0,0 @@
-package com.github.leandroborgesferreira.storyteller.model.action
-
-import com.github.leandroborgesferreira.storyteller.model.story.StoryStep
-
-data class MoveInfo(val storyUnit: StoryStep, val positionFrom: Int, val positionTo: Int)
