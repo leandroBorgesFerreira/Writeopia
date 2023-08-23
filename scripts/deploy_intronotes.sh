14af63b2f6eb5ebb8e7c23f7345ac98ee35e6db5
Leandro Borges Ferreira <lehen01@gmail.com>
2023-08-23 10:57:31 +0200

    deleting scripts

--- scripts/deploy_intronotes.sh
+++
@@ -1,3 +0,0 @@
-./gradlew backend:intronotes:clean backend:intronotes:shadowJar
-
-aws s3 cp backend/intronotes/build/libs/intronotes-0.0.25-SNAPSHOT-all.jar s3://cdk-hnb659fds-assets-352180533495-us-east-1 --profile $AWS_PROFILE
