b47a1c9ba0991b1dadc570c6a8fa08ebc4800d38
Leandro Borges Ferreira <lehen01@gmail.com>
2023-08-27 09:01:43 +0200

    Preview of UnOrderedListItem

--- storyteller/src/main/java/com/github/leandroborgesferreira/storyteller/drawer/content/decoration/ListItemDecoratorDrawer.kt
+++ storyteller/src/main/java/com/github/leandroborgesferreira/storyteller/drawer/content/decoration/ListItemDecoratorDrawer.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
@@ -25,13 +26,14 @@ class ListItemDecoratorDrawer(
     private val textStyle: @Composable () -> TextStyle = {
         LocalTextStyle.current
     },
+    private val startText: String = "-",
     private val stepDrawer: StoryStepDrawer
 ) : StoryStepDrawer {
 
     @Composable
     override fun Step(step: StoryStep, drawInfo: DrawInfo) {
-        Row(modifier = modifier) {
-            Text(text = "- ", style = textStyle())
+        Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+            Text(text = startText, style = textStyle())
 
             Spacer(modifier = Modifier.width(6.dp))
 
