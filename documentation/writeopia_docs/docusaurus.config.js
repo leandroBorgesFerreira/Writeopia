46f68a48809dae6ef933af2fda81b981b7c2acac
Leandro Borges Ferreira <lehen01@gmail.com>
2023-09-14 12:08:33 +0200

    small changes

--- documentation/writeopia_docs/docusaurus.config.js
+++ documentation/writeopia_docs/docusaurus.config.js
@@ -11,14 +11,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://writeopia.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Writeopia', // Usually your GitHub org/user name.
+  organizationName: 'leandroborgesferreira', // Usually your GitHub org/user name.
   projectName: 'Writeopia', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -39,13 +39,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -92,7 +85,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: 'docs/category/getting-started-with-android',
               },
             ],
           },
