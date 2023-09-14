a336883b2a335792e4e3ea9ac7647d18a680b47f
Leandro Borges Ferreira <lehen01@gmail.com>
2023-09-14 14:25:25 +0200

    Deleting build of docusaurus

--- docs/assets/js/6b3b52fe.5b779cde.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwriteopia_docs=self.webpackChunkwriteopia_docs||[]).push([[340],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- docs/assets/js/913eb9e7.a85bde4a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwriteopia_docs=self.webpackChunkwriteopia_docs||[]).push([[374],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
