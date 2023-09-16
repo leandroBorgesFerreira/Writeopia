8d6f3f1ff2cad8cef2732987392456455380ab0b
Leandro Borges Ferreira <lehen01@gmail.com>
2023-09-16 14:24:03 +0200

    Moving documentTracker out of constructor of WriteopiaManager

--- application/features/editor/src/main/java/io/writeopia/editor/di/EditorInjector.kt
+++ application/features/editor/src/main/java/io/writeopia/editor/di/EditorInjector.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import io.writeopia.sdk.manager.DocumentRepository
 import io.writeopia.sdk.manager.WriteopiaManager
 import io.writeopia.sdk.persistence.repository.DocumentRepositoryImpl
-import io.writeopia.sdk.persistence.tracker.OnUpdateDocumentTracker
 import io.writeopia.auth.core.AuthManager
 import io.writeopia.auth.core.di.AuthCoreInjection
 import io.writeopia.editor.NoteEditorViewModel
@@ -24,11 +23,9 @@ class EditorInjector(
 
 
     private fun provideWriteopiaManager(
-        documentRepository: DocumentRepository = provideDocumentRepository(),
         authManager: AuthManager = authCoreInjection.provideAccountManager()
     ) = WriteopiaManager(
-            documentTracker = OnUpdateDocumentTracker(documentRepository),
-            userId = { "provide your user id" }
+            userId = { authManager.getUser().id }
         )
 
     @Composable
@@ -40,16 +37,4 @@ class EditorInjector(
             NoteEditorViewModel(writeopiaManager, documentRepository)
         })
     }
-}
-
-private fun persistenceSample(database: WriteopiaApplicationDatabase) {
-    val documentRepository = DocumentRepositoryImpl(
-        database.documentDao(),
-        database.storyUnitDao()
-    )
-
-    WriteopiaManager(
-        documentTracker = OnUpdateDocumentTracker(documentRepository),
-        userId = { "provide your user id" }
-    )
 }
