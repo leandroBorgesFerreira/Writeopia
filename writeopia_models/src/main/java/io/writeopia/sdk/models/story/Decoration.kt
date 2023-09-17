6b3a9849312d45d4de640a438421a1c4755cb42d
Leandro Borges Ferreira <lehen01@gmail.com>
2023-09-17 15:06:50 +0200

    Kdocs for StoryStep

--- writeopia_models/src/main/java/io/writeopia/sdk/models/story/Decoration.kt
+++ writeopia_models/src/main/java/io/writeopia/sdk/models/story/Decoration.kt
@@ -1,3 +1,8 @@
 package io.writeopia.sdk.models.story
 
+/**
+ * The decoration of the [StoryStep]
+ *
+ * @param backgroundColor The background color.
+ */
 data class Decoration(val backgroundColor: Int? = null)
--- writeopia_models/src/main/java/io/writeopia/sdk/models/story/StoryType.kt
+++ writeopia_models/src/main/java/io/writeopia/sdk/models/story/StoryType.kt
@@ -1,3 +1,10 @@
 package io.writeopia.sdk.models.story
 
+/**
+ * The type of a [StoryStep] you can add new types by adding a unused name and number. It is
+ * recommended to use your package name before the name of the type and use any number above 100.
+ *
+ * @param name Name of the type
+ * @param number The number of the type. Used the identify the type in a fast way.
+ */
 data class StoryType(val name: String, val number: Int)
