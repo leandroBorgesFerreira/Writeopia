f03beb4e3b3846a550d0c70118b6492d2443417b
Leandro Borges Ferreira <lehen01@gmail.com>
2023-09-20 19:28:59 +0200

    Kdocs for PreviewParser

--- writeopia/src/main/java/io/writeopia/sdk/preview/PreviewParser.kt
+++ writeopia/src/main/java/io/writeopia/sdk/preview/PreviewParser.kt
@@ -1,11 +1,23 @@
 package io.writeopia.sdk.preview
 
 import io.writeopia.sdk.models.story.StoryStep
+import io.writeopia.sdk.models.story.StoryType
 import io.writeopia.sdk.models.story.StoryTypes
 
+
+/**
+ * Parser of wanted previous. This class provides a default logic to filter the desired
+ * [StoryType]. It already comes with some default types.
+ *
+ * @param acceptedTypes Set<Int> The number of the accepted types.
+ */
 class PreviewParser(
     private val acceptedTypes: Set<Int> = defaultTypes()
 ) {
+
+    /**
+     * Returns the filtered List of StoryStep
+     */
     fun preview(stories: Iterable<StoryStep>, maxSize: Int = 4): List<StoryStep> {
         var acc = 0
 
