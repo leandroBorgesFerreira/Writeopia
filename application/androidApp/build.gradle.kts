1796dfa144298d5c0ff8a3c2696fbc0b4c2cd35f
Leandro Borges Ferreira <lehen01@gmail.com>
2023-10-01 16:47:05 -0300

    making android app multiplatform

--- application/androidApp/build.gradle.kts
+++ application/androidApp/build.gradle.kts
@@ -1,10 +1,44 @@
-import com.android.build.api.dsl.ManagedVirtualDevice
-
 plugins {
-    alias(libs.plugins.application)
-    id("org.jetbrains.kotlin.android")
+    kotlin("multiplatform")
     id("org.jetbrains.compose")
+    alias(libs.plugins.application)
 }
+
+kotlin {
+    androidTarget()
+
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation(project(":writeopia"))
+                implementation(project(":writeopia_models"))
+                implementation(project(":plugins:writeopia_persistence"))
+                implementation(project(":plugins:writeopia_serialization"))
+                implementation(project(":plugins:writeopia_network"))
+
+                implementation(project(":application:utils"))
+                implementation(project(":application:persistence"))
+                implementation(project(":application:auth_core"))
+                implementation(project(":application:resources"))
+                implementation(project(":application:features:note_menu"))
+                implementation(project(":application:features:editor"))
+                implementation(project(":application:features:auth"))
+                implementation(project(":application:features:account"))
+
+                implementation(libs.androidx.ktx)
+                implementation(libs.appCompat)
+
+                implementation(libs.ktor.client.logging)
+
+                implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+                implementation("androidx.navigation:navigation-compose:2.7.3")
+
+                implementation("androidx.compose.material3:material3:1.1.2")
+            }
+        }
+    }
+}
+
 android {
     namespace = "io.writeopia"
     compileSdk = 34
@@ -39,68 +73,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
-    testOptions {
-        managedDevices {
-            devices {
-                maybeCreate<ManagedVirtualDevice>("pixel6api31")
-                    .apply {
-                        // Use device profiles you typically see in Android Studio.
-                        device = "Pixel 6"
-                        // Use only API levels 27 and higher.
-                        apiLevel = 31
-                        // To include Google services, use "google".
-                        systemImageSource = "aosp"
-                    }
-            }
-        }
-    }
 }
 
-kotlin {
-    sourceSets.all {
-        languageSettings {
-            languageVersion = "1.9"
-        }
-    }
-}
 
 dependencies {
-    implementation(project(":writeopia"))
-    implementation(project(":writeopia_models"))
-    implementation(project(":plugins:writeopia_persistence"))
-    implementation(project(":plugins:writeopia_serialization"))
-    implementation(project(":plugins:writeopia_network"))
-
-    implementation(project(":application:utils"))
-    implementation(project(":application:persistence"))
-    implementation(project(":application:auth_core"))
-    implementation(project(":application:resources"))
-    implementation(project(":application:features:note_menu"))
-    implementation(project(":application:features:editor"))
-    implementation(project(":application:features:auth"))
-    implementation(project(":application:features:account"))
-
-    implementation(libs.androidx.ktx)
-    implementation(libs.appCompat)
-
-    implementation(libs.ktor.client.logging)
-
-    implementation(libs.runtime.compose)
-    implementation(libs.navigation.compose)
-
-    implementation("androidx.compose.material3:material3")
-
-    implementation(platform(libs.androidx.compose.bom))
-
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
