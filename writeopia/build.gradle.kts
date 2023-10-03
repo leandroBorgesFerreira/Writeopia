5c5a2cba4ad32adb16a41e47e560576bcf4e86d6
Leandro Borges Ferreira <lehen01@gmail.com>
2023-10-03 15:19:42 -0300

    Making WriteopiaManagerTest work again

--- writeopia/build.gradle.kts
+++ writeopia/build.gradle.kts
@@ -34,6 +34,7 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                implementation(libs.kotlinx.coroutines.test)
             }
         }
 
