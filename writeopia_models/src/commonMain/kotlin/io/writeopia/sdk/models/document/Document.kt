e84659c8e909a2b2c3de0653d0339fd2bd87260b
Leandro Borges Ferreira <lehen01@gmail.com>
2023-10-05 12:23:39 -0300

    Fixing unit tests

--- writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/document/Document.kt
+++ writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/document/Document.kt
@@ -3,9 +3,10 @@ package io.writeopia.sdk.models.document
 import io.writeopia.sdk.models.story.StoryStep
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import java.util.UUID
 
 data class Document(
-    val id: String,
+    val id: String = UUID.randomUUID().toString(),
     val title: String = "",
     val content: Map<Int, StoryStep> = emptyMap(),
     val createdAt: Instant,
