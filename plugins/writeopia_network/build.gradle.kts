9247b94f0800143eb5bcab32bf7507b7041fd31e
Leandro Borges Ferreira <lehen01@gmail.com>
2023-10-20 23:03:29 +0200

    fixing compilation

--- plugins/writeopia_network/build.gradle.kts
+++ plugins/writeopia_network/build.gradle.kts
@@ -28,14 +28,20 @@ kotlin {
 
                 implementation(libs.kotlinx.serialization.json)
 
-                implementation(libs.ktor.client.okhttp)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.client.serialization.json)
                 implementation(libs.ktor.client.auth)
                 implementation(libs.ktor.client.logging)
 
-                implementation(libs.kotlinx.coroutines.android)
                 implementation(libs.kotlinx.serialization.json)
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                dependsOn(commonMain)
+                implementation(libs.ktor.client.okhttp)
+                implementation(libs.kotlinx.coroutines.android)
             }
         }
     }
