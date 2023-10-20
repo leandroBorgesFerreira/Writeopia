fd63795893ef77691ab2d50cd058d2a52c788710
Leandro Borges Ferreira <lehen01@gmail.com>
2023-10-20 22:50:36 +0200

    Nertwork to KMP

--- plugins/writeopia_network/build.gradle.kts
+++ plugins/writeopia_network/build.gradle.kts
@@ -1,35 +1,43 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("java-library")
-    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlinSerialization)
+    kotlin("multiplatform")
 }
 
 
-rootProject.extra.apply {
-    set("PUBLISH_GROUP_ID", "io.writeopia")
-    set("PUBLISH_ARTIFACT_ID", "writeopia-network")
-    set("PUBLISH_VERSION", libs.versions.writeopia.get())
+//rootProject.extra.apply {
+//    set("PUBLISH_GROUP_ID", "io.writeopia")
+//    set("PUBLISH_ARTIFACT_ID", "writeopia-network")
+//    set("PUBLISH_VERSION", libs.versions.writeopia.get())
+//}
+
+//apply(from = "${rootDir}/scripts/publish-module.gradle")
+
+kotlin {
+    jvm {}
+
+    js(IR) {
+        browser()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":writeopia_models"))
+                implementation(project(":plugins:writeopia_serialization"))
+
+                implementation(libs.kotlinx.serialization.json)
+
+                implementation(libs.ktor.client.okhttp)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.client.serialization.json)
+                implementation(libs.ktor.client.auth)
+                implementation(libs.ktor.client.logging)
+
+                implementation(libs.kotlinx.coroutines.android)
+                implementation(libs.kotlinx.serialization.json)
+            }
+        }
+    }
 }
 
-apply(from = "${rootDir}/scripts/publish-module.gradle")
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-dependencies {
-    implementation(project(":plugins:writeopia_serialization"))
-    implementation(project(":writeopia_models"))
-
-    implementation(libs.kotlinx.serialization.json)
-
-    implementation(libs.ktor.client.okhttp)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.client.serialization.json)
-    implementation(libs.ktor.client.auth)
-    implementation(libs.ktor.client.logging)
-
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.kotlinx.serialization.json)
-}
