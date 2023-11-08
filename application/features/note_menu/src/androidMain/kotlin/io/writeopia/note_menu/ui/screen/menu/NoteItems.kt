9233b59d443b7d5bc54ef1533b8dbe07d082a180
Leandro Borges Ferreira <lehen01@gmail.com>
2023-11-08 21:36:55 +0100

    Moving some classes

--- application/features/note_menu/src/androidMain/kotlin/io/writeopia/note_menu/ui/screen/menu/NoteItems.kt
+++ application/features/note_menu/src/androidMain/kotlin/io/writeopia/note_menu/ui/screen/menu/NoteItems.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.TextStyle
