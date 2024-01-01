188c8c1b702aebc62cb5ea754424775df416e826
Leandro Borges Ferreira <lehen01@gmail.com>
2024-01-01 15:25:09 +0100

    Fixing imports

--- backend/api_editor_spring/src/main/kotlin/io/writeopia/api/editor_spring/Application.kt
+++ backend/api_editor_spring/src/main/kotlin/io/writeopia/api/editor_spring/Application.kt
@@ -5,21 +5,21 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.io.FileInputStream
-import java.util.Properties
-
 
 @SpringBootApplication
 class DocumentEditorApplication
 
 fun main(args: Array<String>) {
+    initFirebase()
+    runApplication<DocumentEditorApplication>(*args)
+}
+
+private fun initFirebase() {
     val option = FirebaseOptions.builder()
         .setProjectId(loadProjectId())
         .setCredentials(GoogleCredentials.getApplicationDefault())
         .build()
     FirebaseApp.initializeApp(option)
-
-    runApplication<DocumentEditorApplication>(*args)
 }
 
 private fun loadProjectId(): String = System.getenv("WRITEOPIA_CLOUD_ID")
