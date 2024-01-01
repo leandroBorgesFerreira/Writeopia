2929a52dfe923b3a5beac861457eaa21aad0919b
Leandro Borges Ferreira <lehen01@gmail.com>
2024-01-01 15:19:47 +0100

    Adding project ID as env var

--- backend/api_editor_spring/src/main/kotlin/io/writeopia/api/editor_spring/Application.kt
+++ backend/api_editor_spring/src/main/kotlin/io/writeopia/api/editor_spring/Application.kt
@@ -5,17 +5,21 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.FileInputStream
 import java.util.Properties
+
 
 @SpringBootApplication
 class DocumentEditorApplication
 
 fun main(args: Array<String>) {
     val option = FirebaseOptions.builder()
-        .setProjectId("[id_here!]]")
+        .setProjectId(loadProjectId())
         .setCredentials(GoogleCredentials.getApplicationDefault())
         .build()
     FirebaseApp.initializeApp(option)
 
     runApplication<DocumentEditorApplication>(*args)
 }
+
+private fun loadProjectId(): String = System.getenv("WRITEOPIA_CLOUD_ID")
