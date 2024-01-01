90107d67695892a9f23939800f2f6603fb6953bb
Leandro Borges Ferreira <lehen01@gmail.com>
2024-01-01 15:22:32 +0100

    delete script

--- backend/create-api.sh
+++
@@ -1,3 +0,0 @@
-gcloud api-gateway api-configs create writeopia-config \
-  --api=writeopia-api --openapi-spec=openapi2-run.yaml \
-  --project=writeopia --backend-auth-service-account=writeopia-service-account@writeopia.iam.gserviceaccount.com
