6c5318cebcea2c4a4b2d76afd35cb9231a3e9f28
Leandro Borges Ferreira <lehen01@gmail.com>
2024-02-09 13:55:14 +0100

    adding firebase

--- backend/editor/api_editor_socket/src/main/kotlin/io/writeopia/Application.kt
+++ backend/editor/api_editor_socket/src/main/kotlin/io/writeopia/Application.kt
@@ -3,6 +3,7 @@ package io.writeopia
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.writeopia.plugins.configureFirebase
 import io.writeopia.plugins.configureRouting
 import io.writeopia.plugins.configureSerialization
 import io.writeopia.plugins.configureSockets
@@ -17,6 +18,7 @@ fun main() {
 }
 
 fun Application.module(byPassAuth: Boolean = false) {
+    configureFirebase()
     configureSockets()
     configureRouting(byPassAuth = byPassAuth)
     configureSerialization()
