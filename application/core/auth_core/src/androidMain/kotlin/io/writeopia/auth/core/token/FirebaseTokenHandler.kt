c5808b1c0345ad65cd85a009b641dce42e9cb6cb
Leandro Borges Ferreira <lehen01@gmail.com>
2024-02-14 21:05:01 +0100

    Loggin token

--- application/core/auth_core/src/androidMain/kotlin/io/writeopia/auth/core/token/FirebaseTokenHandler.kt
+++ application/core/auth_core/src/androidMain/kotlin/io/writeopia/auth/core/token/FirebaseTokenHandler.kt
@@ -1,5 +1,6 @@
 package io.writeopia.auth.core.token
 
+import android.util.Log
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import io.writeopia.sdk.network.oauth.BearerTokenHandler
@@ -8,6 +9,8 @@ import kotlinx.coroutines.tasks.await
 object FirebaseTokenHandler : BearerTokenHandler {
 
     override suspend fun getIdToken(): String? =
-        Firebase.auth.currentUser?.getIdToken(false)?.await()?.token
+        Firebase.auth.currentUser?.getIdToken(false)?.await()?.token?.also {token ->
+            Log.d("FirebaseTokenHandler", "token: $token")
+        }
 
 }
