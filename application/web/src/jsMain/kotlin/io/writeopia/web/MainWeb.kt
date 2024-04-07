6d95c7bba9fd2599d3bb3df77ce86e87cc4c7c6e
Leandro Borges Ferreira <lehen01@gmail.com>
2024-04-07 23:23:37 +0200

    adding undo to web

--- application/web/src/jsMain/kotlin/io/writeopia/web/MainWeb.kt
+++ application/web/src/jsMain/kotlin/io/writeopia/web/MainWeb.kt
@@ -2,11 +2,17 @@ package io.writeopia.web
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.isMetaPressed
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.window.CanvasBasedWindow
 import io.writeopia.note_menu.di.NotesConfigurationInjector
 import io.writeopia.notes.desktop.components.App
 import io.writeopia.sqldelight.di.SqlDelightDaoInjector
 import io.writeopia.ui.drawer.factory.DefaultDrawersJs
+import org.jetbrains.skiko.SkikoKey
 import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -23,7 +29,21 @@ fun CreateAppInMemory(repositoryInjection: SqlDelightDaoInjector) {
     App(
         notesConfigurationInjector = NotesConfigurationInjector.noop(),
         repositoryInjection = repositoryInjection,
-        drawersFactory = DefaultDrawersJs
+        drawersFactory = DefaultDrawersJs,
+        modifier = { writeopiaStateManager ->
+            Modifier.onPreviewKeyEvent { keyEvent ->
+                val shouldHandle = keyEvent.isMetaPressed &&
+                        keyEvent.nativeKeyEvent.key == SkikoKey.KEY_Z &&
+                        keyEvent.type == KeyEventType.KeyDown
+
+                if (shouldHandle) {
+                    writeopiaStateManager.undo()
+                    true
+                } else {
+                    false
+                }
+            }
+        }
     )
 }
 
