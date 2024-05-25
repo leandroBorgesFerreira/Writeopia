86340968c4988e66733fe04c4a22bc7fb52fc364
Leandro Borges Ferreira <lehen01@gmail.com>
2024-05-25 11:53:44 +0200

    Update UiConfigurationInjector.android.kt

--- application/features/note_menu/src/androidMain/kotlin/io/writeopia/note_menu/di/UiConfigurationInjector.android.kt
+++ application/features/note_menu/src/androidMain/kotlin/io/writeopia/note_menu/di/UiConfigurationInjector.android.kt
@@ -6,8 +6,6 @@ import io.writeopia.repository.UiConfigurationRoomRepository
 
 actual class UiConfigurationInjector(private val database: WriteopiaApplicationDatabase) {
 
-
-
     actual fun provideUiConfigurationRepository(): UiConfigurationRepository =
         UiConfigurationRoomRepository(null)
 }
