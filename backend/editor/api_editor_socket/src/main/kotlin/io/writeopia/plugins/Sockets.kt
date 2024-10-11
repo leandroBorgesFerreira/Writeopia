90da702ac1f125d8d57ca269f7ea6eda8887e646
Leandro Borges Ferreira <lehen01@gmail.com>
2024-10-11 17:13:21 +0200

    changing library

--- backend/editor/api_editor_socket/src/main/kotlin/io/writeopia/plugins/Sockets.kt
+++ backend/editor/api_editor_socket/src/main/kotlin/io/writeopia/plugins/Sockets.kt
@@ -1,19 +1,20 @@
 package io.writeopia.plugins
 
-import io.ktor.websocket.*
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
-import io.ktor.server.websocket.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.routing.routing
+import io.ktor.server.websocket.WebSockets
+import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.Frame
+import io.ktor.websocket.readText
+import io.ktor.websocket.send
 import io.writeopia.socket.Connection
-import kotlinx.coroutines.channels.toList
-import java.time.Duration
-import java.util.*
-import kotlin.collections.LinkedHashSet
+import java.util.Collections
 
 fun Application.configureSockets() {
     install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
-        timeout = Duration.ofSeconds(15)
+//        pingPeriod = Duration.ofSeconds(15)
+//        timeout = Duration.ofSeconds(15)
         maxFrameSize = Long.MAX_VALUE
         masking = false
     }
