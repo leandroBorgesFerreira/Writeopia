3f38b8346b3fdeb20638aebf67af78486be41a40
Leandro Borges Ferreira <lehen01@gmail.com>
2024-11-14 13:37:15 +0100

    documentation

---
+++ documentation/writeopia_docs/docs/sdk/overview.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 0
+---
+
+# Overview
+SDK overview
--- writeopia/src/commonMain/kotlin/io/writeopia/sdk/model/story/Selection.kt
+++ writeopia/src/commonMain/kotlin/io/writeopia/sdk/model/story/Selection.kt
@@ -1,11 +1,16 @@
 package io.writeopia.sdk.model.story
 
-data class Selection(val start: Int, val end: Int) {
+data class Selection(val start: SelectionStart, val end: Int) {
     companion object {
-        fun start(): Selection = Selection(0, 0)
+        fun start(): Selection = Selection(SelectionStart.FirstLine(0), 0)
 
-        fun end(): Selection = Selection(Int.MAX_VALUE, Int.MAX_VALUE)
+        fun end(): Selection = Selection(SelectionStart.LastLine(Int.MAX_VALUE), Int.MAX_VALUE)
 
         fun fromPosition(position: Int) = Selection(position, position)
     }
+}
+
+sealed class SelectionStart(val position: Int) {
+    class FirstLine(position: Int): SelectionStart(position)
+    class LastLine(position: Int): SelectionStart(position)
 }
