9320ec39346dc84a448f617c0f7e449a753e7a93
Leandro Borges Ferreira <lehen01@gmail.com>
2024-11-15 09:14:45 +0100

    side by side with topics

--- documentation/writeopia_docs/src/pages/index.tsx
+++ documentation/writeopia_docs/src/pages/index.tsx
@@ -1,42 +1,69 @@
-
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 import React from 'react';
+import { Telescope } from 'lucide-react';
+
+
+function AppSection() {
+  return (
+    <div className={styles.app}>
+      <Heading as="h1" className="hero__title">
+        Application
+      </Heading>
+
+      <div>
+        <p>Keep your documents where you prefer</p>
+      </div>
+      
+
+      <p>Present your ideas with a click</p>
+
+      <div className={styles.buttons}>
+        <Link
+          className="button button--secondary button--lg"
+          to="/docs/overview-app">
+          Documentation
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+
+function SdkSection() {
+  return (
+    <div className={styles.sdk}>
+      <Heading as="h1" className="hero__title">
+        SDK (Developers)
+      </Heading>
+
+      <p>Focus on What Matters</p>
+
+      <p>Robust and very customizable</p>
+
+      <p>Powered by Kotlin</p>
+
+      <div className={styles.buttons}>
+        <Link
+          className="button button--secondary button--lg"
+          to="/docs/overview">
+          Documentation
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <div className={styles.sectionsContainer}>
-        <div className={styles.app}>
-          <Heading as="h1" className="hero__title">
-            Application
-          </Heading>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/overview-app">
-              Documentation
-            </Link>
-          </div>
-        </div>
-      
-        <div className={styles.sdk}>
-          <Heading as="h1" className="hero__title">
-            SDK (Developers)
-          </Heading>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/overview-app">
-              Documentation
-            </Link>
-          </div>
-        </div>
+      <AppSection />
+      <SdkSection />  
     </div>
-    
   );
 }
 
