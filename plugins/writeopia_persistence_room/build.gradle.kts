99eb21913ab0e18eef53e8f01dd34c50881d331a
Leandro Borges Ferreira <lehen01@gmail.com>
2024-11-25 17:19:45 +0100

    Deploying persistence room

--- plugins/writeopia_persistence_room/build.gradle.kts
+++ plugins/writeopia_persistence_room/build.gradle.kts
@@ -1,56 +1,56 @@
-// import com.vanniktech.maven.publish.SonatypeHost
+ import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    kotlin("multiplatform")
     id("com.google.devtools.ksp")
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.dokka)
     alias(libs.plugins.ktlint)
-//    alias(libs.plugins.sonatype.publish)
+    alias(libs.plugins.sonatype.publish)
 }
-//
-// mavenPublishing {
-//    val artifactId = "writeopia-persistence-room"
-//
-//    coordinates(
-//        groupId = "io.writeopia",
-//        artifactId = artifactId,
-//        version = "0.5.0"
-//    )
-//
-//    pom {
-//        name = artifactId
-//        description = "Writeopia plugin - persistence using room"
-//        url = "https://writeopia.io"
-//
-//        developers {
-//            developer {
-//                id = "leandroBorgesFerreira"
-//                name = "Leandro Borges Ferreira"
-//                url = "https://github.com/leandroBorgesFerreira"
-//            }
-//        }
-//
-//        licenses {
-//            license {
-//                name = "The Apache Software License, Version 2.0"
-//                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-//            }
-//        }
-//
-//        scm {
-//            connection = "scm:git@github.com:leandroBorgesFerreira/Writeopia.git"
-//            developerConnection = "scm:git:ssh://github.com/leandroBorgesFerreira/Writeopia.git"
-//            url = "https://github.com/leandroBorgesFerreira/Writeopia"
-//        }
-//    }
-//
-//    signAllPublications()
-//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
-// }
+
+ mavenPublishing {
+    val artifactId = "writeopia-persistence-room"
+
+    coordinates(
+        groupId = "io.writeopia",
+        artifactId = artifactId,
+        version = "0.5.0"
+    )
+
+    pom {
+        name = artifactId
+        description = "Writeopia plugin - persistence using room"
+        url = "https://writeopia.io"
+
+        developers {
+            developer {
+                id = "leandroBorgesFerreira"
+                name = "Leandro Borges Ferreira"
+                url = "https://github.com/leandroBorgesFerreira"
+            }
+        }
+
+        licenses {
+            license {
+                name = "The Apache Software License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        scm {
+            connection = "scm:git@github.com:leandroBorgesFerreira/Writeopia.git"
+            developerConnection = "scm:git:ssh://github.com/leandroBorgesFerreira/Writeopia.git"
+            url = "https://github.com/leandroBorgesFerreira/Writeopia"
+        }
+    }
+
+    signAllPublications()
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
+ }
 
 android {
-    namespace = "io.writeopia.sdk.persistence"
+    namespace = "io.writeopia.sdk.persistence_room"
     compileSdk = 34
 
     defaultConfig {
@@ -78,23 +78,63 @@ android {
     }
 }
 
+
+kotlin {
+    jvm {}
+
+    androidTarget()
+
+//    js(IR) {
+//        browser()
+//        binaries.library()
+//    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":writeopia"))
+                implementation(project(":writeopia_models"))
+                implementation(project(":plugins:writeopia_persistence_core"))
+
+                implementation(libs.kotlinx.datetime)
+
+                implementation(libs.room.runtime)
+                implementation(libs.room.ktx)
+                implementation(libs.room.paging)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+                implementation(libs.kotlinx.coroutines.test)
+            }
+        }
+    }
+}
+
 dependencies {
-    implementation(project(":writeopia"))
-    implementation(project(":writeopia_models"))
-    implementation(project(":plugins:writeopia_persistence_core"))
-
-    implementation(libs.kotlinx.datetime)
-
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
-    ksp(libs.room.compiler)
-    implementation(libs.room.paging)
-
-    testImplementation(libs.kotlin.test)
-    testImplementation(libs.room.testing)
-
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    dependencies {
+        add("kspAndroid", libs.room.compiler)
+    }
 }
 
 tasks.dokkaHtmlPartial {
