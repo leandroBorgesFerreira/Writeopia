0d9e73a2c8d113220ebc10d5f7b9197c80f215f9
Leandro Borges Ferreira <lehen01@gmail.com>
2024-12-01 13:17:25 +0100

    Deleting unused code

--- application/common_flows/wide_screen_common/build.gradle.kts
+++ application/common_flows/wide_screen_common/build.gradle.kts
@@ -14,6 +14,17 @@ kotlin {
         binaries.library()
     }
 
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
