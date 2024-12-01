3d883c4f42a44bbcda24cb5e0c2ace2e5e82c9da
Leandro Borges Ferreira <lehen01@gmail.com>
2024-12-01 12:13:02 +0100

    fix(UI): Fixing title style (#265)
    * Fixing title style
    
    * Ktlint fix

--- writeopia_ui/src/commonMain/kotlin/io/writeopia/ui/model/DrawConfig.kt
+++ writeopia_ui/src/commonMain/kotlin/io/writeopia/ui/model/DrawConfig.kt
@@ -1,6 +1,24 @@
 package io.writeopia.ui.model
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+
 data class DrawConfig(
+    val titleStyle: @Composable () -> TextStyle = {
+        MaterialTheme.typography.displaySmall.copy(
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    },
+    val titlePlaceHolderStyle: @Composable () -> TextStyle = {
+        MaterialTheme.typography.displaySmall.copy(
+            fontWeight = FontWeight.Bold,
+            color = Color.LightGray
+        )
+    },
     val textDrawerStartPadding: Int = 8,
     val textVerticalPadding: Int = 0,
     val codeBlockStartPadding: Int = 8,
