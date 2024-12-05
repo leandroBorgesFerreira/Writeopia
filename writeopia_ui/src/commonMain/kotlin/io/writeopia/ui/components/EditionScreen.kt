610f4001e55db08e84f8dd073676ac9b8a03bc5e
Leandro Borges Ferreira <lehen01@gmail.com>
2024-12-05 11:16:44 +0100

    Adding checkbox and list item in the multi selection options (#268)

--- writeopia_ui/src/commonMain/kotlin/io/writeopia/ui/components/EditionScreen.kt
+++ writeopia_ui/src/commonMain/kotlin/io/writeopia/ui/components/EditionScreen.kt
@@ -1,21 +1,19 @@
 package io.writeopia.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DeleteOutline
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import io.writeopia.ui.icons.WrSdkIcons
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 // Here
@@ -24,32 +22,60 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun EditionScreen(
     modifier: Modifier = Modifier,
+    checkboxClick: () -> Unit = {},
+    listItemClick: () -> Unit = {},
     onDelete: () -> Unit = {},
     onClose: () -> Unit = {},
 ) {
-    Box(modifier = modifier) {
+    Row(modifier = modifier) {
         Icon(
             modifier = Modifier
-                .align(Alignment.Center)
+                .clickable(onClick = checkboxClick)
+                .weight(1F)
                 .size(50.dp)
-                .padding(10.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .clickable(onClick = onDelete),
-            imageVector = Icons.Default.DeleteOutline,
-            contentDescription = "Delete",
+                .padding(12.dp)
+                .clip(RoundedCornerShape(8.dp)),
+            imageVector = WrSdkIcons.checkbox,
+            contentDescription = "Checkbox",
 //            contentDescription = stringResource(R.string.delete),
             tint = MaterialTheme.colorScheme.onPrimary
         )
 
         Icon(
             modifier = Modifier
+                .clickable(onClick = listItemClick)
                 .size(50.dp)
-                .align(Alignment.CenterEnd)
+                .weight(1F)
                 .padding(12.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .clickable(onClick = onClose),
-            imageVector = Icons.Outlined.Close,
+                .clip(RoundedCornerShape(8.dp)),
+            imageVector = WrSdkIcons.list,
+            contentDescription = "List item",
+//            contentDescription = stringResource(R.string.delete),
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
+
+        Icon(
+            modifier = Modifier
+                .clickable(onClick = onDelete)
+                .weight(1F)
+                .size(50.dp)
+                .padding(12.dp)
+                .clip(RoundedCornerShape(8.dp)),
+            imageVector = Icons.Default.DeleteOutline,
             contentDescription = "Delete",
+//            contentDescription = stringResource(R.string.delete),
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
+
+
+        Icon(
+            modifier = Modifier
+                .clickable(onClick = onClose)
+                .size(50.dp)
+                .padding(12.dp)
+                .clip(RoundedCornerShape(8.dp)),
+            imageVector = WrSdkIcons.close,
+            contentDescription = "List item",
 //            contentDescription = stringResource(R.string.delete),
             tint = MaterialTheme.colorScheme.onPrimary
         )
---
+++ writeopia_ui/src/commonMain/kotlin/io/writeopia/ui/icons/all/List.kt
@@ -0,0 +1,115 @@
+package io.writeopia.ui.icons.all
+/*
+* Converted using https://composables.com/svgtocompose
+*/
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+internal val List: ImageVector
+    get() {
+        if (_List != null) {
+            return _List!!
+        }
+        _List = ImageVector.Builder(
+            name = "List",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(3f, 12f)
+                horizontalLineToRelative(0.01f)
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(3f, 18f)
+                horizontalLineToRelative(0.01f)
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(3f, 6f)
+                horizontalLineToRelative(0.01f)
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(8f, 12f)
+                horizontalLineToRelative(13f)
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(8f, 18f)
+                horizontalLineToRelative(13f)
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(8f, 6f)
+                horizontalLineToRelative(13f)
+            }
+        }.build()
+        return _List!!
+    }
+
+private var _List: ImageVector? = null
