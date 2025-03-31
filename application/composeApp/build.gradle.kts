afc6b2fd1488110ee26255d44009322f17174dfa
Leandro Ferreira <email@email.com>
2025-03-31 18:48:38 +0200

    Version bump

--- application/composeApp/build.gradle.kts
+++ application/composeApp/build.gradle.kts
@@ -140,7 +140,7 @@ compose.desktop {
             }
 
             linux {
-                packageVersion = "0.34.0"
+                packageVersion = "0.35.0"
                 iconFile.set(iconsRoot.resolve("icon-linux.png"))
             }
 
